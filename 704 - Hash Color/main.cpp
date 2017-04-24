63dc0548dd1fc8f1889ae62b9736d05a5e9b006a
Dalai Ribeiro <dalairibeiro@Dalais-MacBook-Air.local>
2017-04-24 01:42:51 -0300

    Initial Commit

---
+++ 704 - Hash Color/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  704 - Hash Color
+//
+//  Created by Dalai Ribeiro on 24/04/17.
+//  Copyright © 2017 Dalai Ribeiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
